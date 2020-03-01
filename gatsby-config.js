90592dbd0e54bbf62512d2fc161547e041663b06
Yemachu <yemachu@gmail.com>
2020-03-01 13:01:43 +0100

    Changed some values of the default configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Neo New Card maker`,
+    description: `Create custom trading card game cards online.`,
+    author: `Yemachu`,
   },
   pathPrefix: `/site`,
   plugins: [
@@ -20,13 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Neo New Card maker`,
+        display: `standalone`,
+        short_name: `Card maker`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#3f51b5`,
+        theme_color: `#3f51b5`,
+        theme_color_in_head: false,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Icon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
