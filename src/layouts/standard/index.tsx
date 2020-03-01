783aca8ff3e9e316dc0958af6b3799e6540e1d4a
Yemachu <yemachu@gmail.com>
2020-03-01 13:51:12 +0100

    Created a layout with drawer.
    The drawer is persistent on wide screens, and togglable on smaller screens.

---
+++ src/layouts/standard/index.tsx
@@ -0,0 +1,167 @@
+import * as React from "react";
+
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton,
+
+	ThemeProvider,
+	CssBaseline,
+
+	createMuiTheme,
+	useMediaQuery,
+
+	createStyles,
+	makeStyles,
+	Theme,
+	Paper,
+	Tabs,
+	Tab,
+
+} from "@material-ui/core";
+
+import { Route } from "../../routes";
+
+import {
+	LanguageSelector,
+
+} from "../../components";
+
+import {
+	indigo,
+	amber,
+
+} from "@material-ui/core/colors"
+
+import {
+	Menu as MenuIcon,
+	Add as AddIcon,
+
+} from "@material-ui/icons";
+
+import { Helmet } from "react-helmet";
+
+import { Link } from "gatsby"
+import { ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
+
+
+import Drawer from "./Drawer";
+
+const drawerWidth = 350;
+
+const useStyles = makeStyles((theme: Theme) => {
+	return createStyles({
+		appBar: {
+			[theme.breakpoints.up("md")]: {
+				width: `calc(100% - ${drawerWidth}px)`,
+				marginLeft: drawerWidth,
+			},
+		},
+		title: {
+			flexGrow: 1,
+
+		},
+		menuButton: {
+			marginRight: theme.spacing(2),
+			[theme.breakpoints.up("md")]: {
+				display: "none"
+			},
+		},
+
+		content: {
+			flexGrow: 1,
+			padding: theme.spacing(2),
+		},
+		root: {
+			display: "flex",
+		},
+		toolbar: {
+
+		}
+
+	});
+});
+
+const DARK_PALETTE: ThemeOptions = {
+	palette: {
+		type: "dark",
+		primary: {
+			main: indigo[200],
+		},
+		secondary: {
+			main: amber[200],
+		},
+	},
+};
+
+const LIGHT_PALETTE: ThemeOptions = {
+	palette: {
+		type: "light",
+		primary: indigo,
+		secondary: amber,
+	},
+}
+
+export interface LayoutProps
+{
+	readonly children: JSX.Element | JSX.Element[];
+	readonly routes?: readonly Route[];
+	readonly location?: Location;
+}
+
+
+export default function (props: LayoutProps) {
+	const classes = useStyles();
+	const prefersDark = useMediaQuery("");
+	const theme = React.useMemo(() => {
+		return createMuiTheme(prefersDark ? DARK_PALETTE : LIGHT_PALETTE);
+	}, [prefersDark]);
+
+	const [drawerOpen, setDrawerOpen] = React.useState(false);
+	const activeTab = props.routes?.findIndex((route)=>route.path==props.location?.pathname) || 0;
+	const tabs = React.useMemo(()=>{
+		return props.routes?.map((route)=>{
+			return <Tab label={route.key} component={Link} to={route.path} />;
+		})
+
+	}, [props.routes]);
+
+	return <ThemeProvider theme={theme}>
+		<CssBaseline />
+		<Helmet>
+			<meta name="theme-color" content={prefersDark ? indigo[200] : indigo[500]} />
+		</Helmet>
+		<div className={classes.root}>
+
+			<AppBar className={classes.appBar} color={prefersDark ? "default" : "primary"}>
+				<Toolbar>
+					<IconButton
+						edge="start"
+						color="inherit"
+						onClick={(e) => { setDrawerOpen(!drawerOpen); }}
+						className={classes.menuButton}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" component="h1" className={classes.title} noWrap >Neo Card Maker</Typography>
+					<LanguageSelector language="en" setLanguage={() => { }} />
+				</Toolbar>
+
+				<Paper square>
+					<Tabs value={activeTab} variant="scrollable">
+						{tabs}
+					</Tabs>
+				</Paper>
+			</AppBar>
+			<Drawer isOpen={drawerOpen} setIsOpen={setDrawerOpen} />
+			
+			<main className={classes.content}>
+				<Toolbar />
+				<Tabs value={0} />
+
+				{props.children}
+			</main>
+		</div>
+	</ThemeProvider>
+}
