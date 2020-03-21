9737d4ad3ad889341866828bebd64b0cb5dbbf17
Yemachu <yemachu@gmail.com>
2020-03-21 17:37:25 +0100

    Used the redux store to edit the card.

--- src/ygo/index.tsx
+++ src/ygo/index.tsx
@@ -1,17 +1,31 @@
 import * as React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import { Grid } from "@material-ui/core";
+import { Grid, Button, ButtonGroup } from "@material-ui/core";
 import { CardRenderer } from "./views";
+import { reducer } from "./models/Card";
+import { randomize, illegalize } from "./models/SerialNumber/actions";
+
+const store = createStore(reducer);
+store.dispatch({type: ""});
 
 export default function YuGiOh()
 {
-	return <Grid container spacing={1}>
-		<Grid item xs={12} sm={6} md={12} lg={6}>
-			<CardRenderer />
-		</Grid>
 
-		<Grid item xs={12} sm={6} md={12} lg={6}>
-			Yu-Gi-Oh! Card maker.
+	return <Provider store={store}>
+		<Grid container spacing={1}>
+			<Grid item xs={12} sm={6} md={12} lg={6}>
+				<CardRenderer />
+			</Grid>
+
+			<Grid item xs={12} sm={6} md={12} lg={6}>
+				Yu-Gi-Oh! Card maker.
+				<ButtonGroup>
+					<Button onClick={()=>store.dispatch(randomize())}>Randomize</Button>
+					<Button onClick={()=>store.dispatch(illegalize())}>Illegalize</Button>
+				</ButtonGroup>
+			</Grid>
 		</Grid>
-	</Grid>
+	</Provider>
 }
