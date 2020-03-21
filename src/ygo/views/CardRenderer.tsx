bcbf651a25f025c55b2de00a627e00f079acfd8c
Yemachu <yemachu@gmail.com>
2020-03-21 17:38:04 +0100

    Made the renderer show the serial number.

--- src/ygo/views/CardRenderer.tsx
+++ src/ygo/views/CardRenderer.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
+import { Card, SerialNumber } from "../models";
 
 interface Props
 {
@@ -7,13 +9,26 @@ interface Props
 
 export default function CardRenderer({}: Props)
 {
+	const serialNumber = useSelector<Card, SerialNumber>(card=> card.serialNumber);
+
 	const render = React.useCallback(function(canvas: HTMLCanvasElement | null)
 	{
-		const ctx = canvas?.getContext("2d");
+		if (!canvas){ return; }
+		
+		const ctx = canvas.getContext("2d");
 		if (!ctx){ return; }
 
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.fillText("Rendered a card.", 0, 16);
-	}, []);
+		ctx.fillText(serialNumber, 0, 32);
+	}, [serialNumber]);
 
-	return <canvas width={420} height={610} ref={render} />;
+	return <canvas
+		width={420}
+		height={610}
+		style={{
+			maxWidth: "100%"
+		}}
+		ref={render}
+	/>;
 }
