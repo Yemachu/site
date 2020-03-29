d8e4d07909dd59adb60c616173ffa80801920c1c
Yemachu <yemachu@gmail.com>
2020-03-29 14:07:13 +0200

    Made Level's value a sub-property, and added other properties.
    This includes whether the level should be mirrored and what style of stars should be used.

--- src/ygo/models/Level/type.ts
+++ src/ygo/models/Level/type.ts
@@ -1,4 +1,27 @@
-export type Level = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+type AllowedValues = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
-export const MAX_VALUE = 12;
-export const MIN_VALUE = 0;
+enum Variant
+{
+	DEFAULT = "default",
+	NORMAL = "normal",
+	DARK_SYNCHRO = "dark_synchro",
+	XYZ = "xyz"
+}
+
+export default interface Level {
+	readonly value: AllowedValues;
+	readonly variant: Variant;
+	readonly mirrored: boolean;
+}
+
+const MAX_VALUE = 12;
+const MIN_VALUE = 0;
+
+export {
+	AllowedValues,
+	Level,
+	Variant,
+
+	MAX_VALUE,
+	MIN_VALUE,
+} 
