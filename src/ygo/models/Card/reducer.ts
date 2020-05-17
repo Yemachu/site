5d218085ee596a847336c830fa1ac279dc9df29b
Yemachu <yemachu@gmail.com>
2020-05-17 13:06:13 +0200

    Added the new data types as properties to `Card`.
    These properties were originally missing, as they were not implemented at that time.

--- src/ygo/models/Card/reducer.ts
+++ src/ygo/models/Card/reducer.ts
@@ -1,21 +1,27 @@
 import { combineReducers } from "redux";
 
 import { reducer as AttributeReducer } from "../Attribute";
-import { reducer as NameReducer } from "../Name";
+import { reducer as EffectReducer } from "../Effect";
+import { reducer as ImageReducer } from "../Image";
 import { reducer as LevelReducer } from "../Level";
+import { reducer as LinkReducer } from "../Link";
+import { reducer as MonsterTypeReducer } from "../MonsterType";
+import { reducer as NameReducer } from "../Name";
 import { reducer as PendulumReducer } from "../Pendulum";
 import { reducer as SerialNumberReducer } from "../SerialNumber";
-import { reducer as LinkReducer } from "../Link";
 
 import Card from "./type";
 
 const reducer = combineReducers<Card>({
 	attribute: AttributeReducer,
-	name: NameReducer,
+	effect: EffectReducer,
+	image: ImageReducer,
 	level: LevelReducer,
+	link: LinkReducer,
+	monsterType: MonsterTypeReducer,
+	name: NameReducer,
 	pendulum: PendulumReducer,
 	serialNumber: SerialNumberReducer,
-	link: LinkReducer,
 });
 
 export default reducer;
