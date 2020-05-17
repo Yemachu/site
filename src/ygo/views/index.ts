1461ab50942dfe4d1c8a0748cb0f74606b6f22db
Yemachu <yemachu@gmail.com>
2020-05-17 15:24:13 +0200

    Updated the index file, allowing for nicer imports.

--- src/ygo/views/index.ts
+++ src/ygo/views/index.ts
@@ -1,5 +1,27 @@
+import AttributeEditor from "./AttributeEditor";
+import BackrowTypeEditor from "./BackrowTypeEditor";
 import CardRenderer from "./CardRenderer";
+import EffectEditor from "./EffectEditor";
+import ImageEditor from "./ImageEditor";
+import LevelEditor from "./LevelEditor";
+import LinkEditor from "./LinkEditor";
+import MonsterTypeEditor from "./MonsterTypeEditor";
+import NameEditor from "./NameEditor";
+import PendulumEditor from "./PendulumEditor";
+import SerialNumberEditor from "./SerialNumberEditor";
+import StatsEditor from "./StatsEditor";
 
 export {
+	AttributeEditor,
+	BackrowTypeEditor,
 	CardRenderer,
+	EffectEditor,
+	ImageEditor,
+	LevelEditor,
+	LinkEditor,
+	MonsterTypeEditor,
+	NameEditor,
+	PendulumEditor,
+	SerialNumberEditor,
+	StatsEditor,
 }
