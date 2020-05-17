4971eef0c53b6f44633c47b336b91acb96f224e1
Yemachu <yemachu@gmail.com>
2020-05-17 15:19:00 +0200

    Initial setup for editing pendulum effects and values.
    The correct bindings still need to instantiated.

---
+++ src/ygo/views/PendulumEditor.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+	Grid,
+	TextField,
+	Checkbox,
+	FormControlLabel,
+
+	ExpansionPanel,
+	ExpansionPanelSummary,
+	ExpansionPanelDetails,
+
+} from "@material-ui/core";
+
+import {
+	ExpandMore as ExpandMoreIcon,
+
+} from "@material-ui/icons";
+
+function stopPropagation(e: React.MouseEvent | React.FocusEvent): void
+{
+	e.stopPropagation();
+}
+
+export default function PendulumEditor(): JSX.Element
+{
+	const dispatch = useDispatch();
+	const value = useSelector(state => state);
+
+	return React.useMemo(function(){
+		return <ExpansionPanel>
+			<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+				<FormControlLabel
+					onClick={stopPropagation}
+					onFocus={stopPropagation}
+					control={<Checkbox/>}
+					label="Pendulum"
+				/>
+			</ExpansionPanelSummary>
+			<ExpansionPanelDetails>
+				<Grid container spacing={2}>
+					<Grid item xs={6}>
+						<TextField
+						value={0}
+						onChange={(evt)=>{/*dispatch(actions.);*/}}
+						fullWidth
+						/>
+					</Grid>
+					<Grid item xs={6}>
+						<TextField
+						value={0}
+						onChange={(evt) => {/* dispatch(actions.);*/}}
+						fullWidth />
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							onChange={(evt) => {/* dispatch(actions.);*/}}
+							multiline
+							fullWidth
+							/>
+					</Grid>
+				</Grid>
+			</ExpansionPanelDetails>
+		</ExpansionPanel>
+
+
+	}, [value, dispatch]);
+
+}
