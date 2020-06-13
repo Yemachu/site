3d28c9824444b4e2d0fd9f39281466cdaee8caaf
Yemachu <yemachu@gmail.com>
2020-06-13 17:19:40 +0200

    Hooked up the rarity editor with the data model.

--- src/ygo/views/RarityEditor.tsx
+++ src/ygo/views/RarityEditor.tsx
@@ -5,11 +5,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { TextField, MenuItem } from "@material-ui/core";
 
 import { Rarity } from "../models";
+import { actions } from "../models/Rarity";
+
+interface RarityProvider
+{
+	readonly rarity: Rarity;
+}
 
 export default function RarityEditor(): JSX.Element
 {
 	const { t } = useTranslation();
-	const value = useSelector(()=>null);
+	const value = useSelector((container: RarityProvider) => container.rarity);
 	const dispatch = useDispatch();
 
 	return React.useMemo(function()
@@ -18,8 +24,10 @@ export default function RarityEditor(): JSX.Element
 			fullWidth
 			InputLabelProps={{shrink: true}}
 			label={t("ygo:ui.rarity", { defaultValue: "Rarity" })}
+			onChange={(evt)=>{dispatch(actions.set(evt.target.value as Rarity)); console.log(evt.target.value);}}
 			select
 			size="small"
+			value={value}
 			variant="outlined"
 		>
 			<MenuItem value={Rarity.Common}>Common</MenuItem>
