95abd628e40f3cef25467de4adaf618df64637d7
Yemachu <yemachu@gmail.com>
2020-06-28 12:49:31 +0200

    Made `template` changes lasting.

--- src/ygo/views/TemplateEditor.tsx
+++ src/ygo/views/TemplateEditor.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Type as Template, actions } from "../models/Template";
 
 import { TextField, MenuItem, Divider, } from "@material-ui/core";
+
+interface TemplateProvider
+{
+	readonly template: Template;
+}
 
 export default function TemplateEditor(): JSX.Element
 {
 	const dispatch = useDispatch();
-	const value = useSelector(()=>null);
+	const value = useSelector((provider: TemplateProvider)=>provider.template);
 
 	return React.useMemo(function()
 	{
@@ -14,24 +20,25 @@ export default function TemplateEditor(): JSX.Element
 			fullWidth
 			InputLabelProps={{shrink: true}}
 			label="Template"
+			value={value}
+			onChange={(evt) => { dispatch(actions.set(evt.target.value as Template)); }}
 			select
 			size="small"
 			variant="outlined"
 		>
-			<MenuItem>Normal</MenuItem>
-			<MenuItem>Effect</MenuItem>
-			<MenuItem>Ritual</MenuItem>
+			<MenuItem value={Template.NORMAL}>Normal</MenuItem>
+			<MenuItem value={Template.EFFECT}>Effect</MenuItem>
+			<MenuItem value={Template.RITUAL}>Ritual</MenuItem>
 			<Divider component="li" />
-			<MenuItem>Fusion</MenuItem>
-			<MenuItem>Synchro</MenuItem>
-			<MenuItem>Xyz</MenuItem>
-			<MenuItem>Link</MenuItem>
+			<MenuItem value={Template.FUSION}>Fusion</MenuItem>
+			<MenuItem value={Template.SYNCHRO}>Synchro</MenuItem>
+			<MenuItem value={Template.XYZ}>Xyz</MenuItem>
+			<MenuItem value={Template.LINK}>Link</MenuItem>
 			<Divider component="li" />
-			<MenuItem>Spell</MenuItem>
-			<MenuItem>Trap</MenuItem>
+			<MenuItem value={Template.SPELL}>Spell</MenuItem>
+			<MenuItem value={Template.TRAP}>Trap</MenuItem>
 			<Divider component="li" />
-			<MenuItem>Token</MenuItem>
-			<MenuItem></MenuItem>
+			<MenuItem value={Template.TOKEN}>Token</MenuItem>
 		</TextField>
 
 	}, [dispatch, value]);
