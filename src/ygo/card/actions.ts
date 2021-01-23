f3809e8afcb33a944aa60eb7a18705942f689efd
Yemachu <yemachu@gmail.com>
2021-01-23 12:10:18 +0100

    Made the template and rarity editable.

--- src/ygo/card/actions.ts
+++ src/ygo/card/actions.ts
@@ -1,17 +1,25 @@
 import { actions as name } from "./name";
 import { actions as level } from "./level";
+import { actions as attribute } from "./attribute";
+import { actions as template } from "./template";
 
 export type RootAction =
 | name.RootAction
 | level.RootAction
+| attribute.RootAction
+| template.RootAction
 ;
 
 export { 
   name, 
   level,
+  attribute,
+  template,
 };
 
 export const all = [
   ...name.all,
   ...level.all,
+  ...attribute.all,
+  ...template.all,
 ];
--- src/ygo/card/model.ts
+++ src/ygo/card/model.ts
@@ -3,14 +3,17 @@ import { combineReducers } from "redux";
 import { name, Name } from "./name";
 import { level, Level } from "./level";
 import { attribute, Attribute } from "./attribute";
+import { template, Template } from "./template";
 
 export type Card = {
   readonly name: Name;
   readonly level: Level;
   readonly attribute: Attribute;
+  readonly template: Template;
 };
 export const card = combineReducers<Card>({
   name,
   level,
   attribute,
+  template,
 });
