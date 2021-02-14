90f7227582cdcff6c76819aef3330ac2448daf6b
Yemachu <yemachu@gmail.com>
2021-02-14 13:17:13 +0100

    Updated the rendering of the card component.

--- src/ygo/card/component.tsx
+++ src/ygo/card/component.tsx
@@ -18,13 +18,11 @@ export type CardEditorProps = {
 
 export const CardEditor = ({ card }: CardEditorProps): JSX.Element =>
 {
-  return <Box p={2}>
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}><TemplateEditor template={card?.template} /></Grid>
-      <Grid item xs={12} sm={6}><RarityEditor rarity={card?.rarity} disabled={!card} /></Grid>
-      <Grid item xs={12} sm={8}><NameEditor name={card?.name} /></Grid>
-      <Grid item xs={12} sm={4}><AttributeEditor attribute={card?.attribute} /></Grid>
-      <Grid item xs={12}><LevelEditor level={card?.level} /></Grid>
-    </Grid>
-  </Box>
+  return <Grid container spacing={2}>
+    <Grid item xs={12} sm={6}><TemplateEditor template={card?.template} /></Grid>
+    <Grid item xs={12} sm={6}><RarityEditor rarity={card?.rarity} disabled={!card} /></Grid>
+    <Grid item xs={12} sm={8}><NameEditor name={card?.name} /></Grid>
+    <Grid item xs={12} sm={4}><AttributeEditor attribute={card?.attribute} /></Grid>
+    <Grid item xs={12}><LevelEditor level={card?.level} /></Grid>
+  </Grid>
 }
